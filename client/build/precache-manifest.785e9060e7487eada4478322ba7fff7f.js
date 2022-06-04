d604ca90d82c3603d0af6403a8384355a4c15e79
walterunderwood <walonsounderwood@gmail.com>
2022-06-03 20:25:27 -0700

    Create Initial Stucture

---
+++ client/build/precache-manifest.785e9060e7487eada4478322ba7fff7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d8bc1c5396bf5e92935e8c3eda068f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db1e2b4b56d8f36b771e",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "488381eecdc35bc9c03e",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "db1e2b4b56d8f36b771e",
+    "url": "/static/js/2.a252678e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.a252678e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488381eecdc35bc9c03e",
+    "url": "/static/js/main.46b908f0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.46b908f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
