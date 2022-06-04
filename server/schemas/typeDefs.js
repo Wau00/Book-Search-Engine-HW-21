66133dd2762d22ccda56592505ac2e47ec565c11
walterunderwood <walonsounderwood@gmail.com>
2022-06-03 21:04:16 -0700

    Create typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Book {
+    _id: ID
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    forSale: String
+    link: String
+    title: String
+}
+type Auth {
+    token: ID!
+    user: User
+
+}
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]        
+}
+
+input SavedBookInput {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    forSale: String
+    link: String
+    title: String
+}
+
+type Query {
+    me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: SavedBookInput): User
+    removeBook(bookId: String!): User
+}
+`;
+
+module.exports = typeDefs;
