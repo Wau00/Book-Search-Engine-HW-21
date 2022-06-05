2a2c4b856f651a712abbebb529619538992c318c
walterunderwood <walonsounderwood@gmail.com>
2022-06-04 20:46:50 -0700

    Debugging Pages and Components

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
@@ -8,6 +7,7 @@ import Auth from '../utils/auth';
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
@@ -15,8 +15,17 @@ const SignupForm = () => {
 
   const [addUser, { error, data }] = useMutation(ADD_USER);
 
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
+
     setUserFormData({ ...userFormData, [name]: value });
   };
 
